fec54665510cc4c8071f1999e7be8f68d4b41a34
Kaiki <kaiki.machado@portalsesisp.org.br>
2024-04-12 11:13:02 -0300

    apisaude

---
+++ Models/Entrega.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System;
+using System.Collections.Generic;
+
+namespace apisaude.Models;
+
+public partial class Entrega
+{
+    public int CodEntrega { get; set; }
+
+    public int CodPaciente { get; set; }
+
+    public int CodMatmed { get; set; }
+
+    public DateOnly? DataEntrega { get; set; }
+
+    public DateOnly? DataProEntrega { get; set; }
+
+    [JsonIgnore]
+    public virtual MatMed? CodMatmedNavigation { get; set; }
+    [JsonIgnore]
+    public virtual Paciente? CodPacienteNavigation { get; set; }
+}
